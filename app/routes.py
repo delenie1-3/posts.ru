7f7f2755353a2e6572685050f8685b9bbe49e26d
ASUS <delenie1.3@gmail.com>
2021-09-26 14:53:49 +0500

    add templates % index.html

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,22 @@
+#-*- coding: utf-8 -*-
 from app import app
+
+'''@app.route('/')
+@app.route('/index')
+def index():
+    return "Проверка работоспособности Flask"'''
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Проверка работоспособности Flask"
+    user = {'username':'web-dev'}
+    return '''
+    <html>
+        <head>
+            <title>Домашная страница - Posts</title>
+        </head>
+        <body>
+            <h1>Приветствую, ''' + user['username'] + '''!</h1>
+        </body>
+    </html>
+    '''
