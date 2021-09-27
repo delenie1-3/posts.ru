f4ff78601a1e1ea40debdb57eac4297a7ccb1321
ASUS <delenie1.3@gmail.com>
2021-09-27 11:57:16 +0500

    add model users

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from app import db
 
-class User(db.Model):
+class Users(db.Model):#модель пользователя
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
